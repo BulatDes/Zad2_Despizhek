fc5527faf27f143aa879f1108a612cc827870dd1
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-06-16 14:08:08 +0500

    Add files via upload

---
+++ zad2/PhoneBook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace zad2
+{
+    internal class PhoneBook
+    {
+        public DataTable table = new DataTable();
+        public List<Contact> list = new List<Contact>();
+
+        public void AddContact (string name, string phone)
+        {
+            table.Rows.Add(name, phone);
+        }
+
+        public void ReloadList ()
+        {
+            foreach (DataRow value in table.Rows)
+            {
+                Contact contact = new Contact();
+                contact.Name = (string) value[0];
+                contact.Phone = (string) value[1];
+                list.Add(contact);
+            }
+        }
+    }
+}
---
+++ zad2/PhoneBookLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using System.IO;
+using System.Data;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace zad2
+{
+    class PhoneBookLoader
+    {
+            public void Load (PhoneBook phoneBook, string fileName)
+            {
+
+                phoneBook.table.Columns.Add("Name");
+                phoneBook.table.Columns.Add("Telephone");
+                FileStream str = File.Open(fileName, FileMode.Open, FileAccess.Read);
+                IExcelDataReader reader = ExcelReaderFactory.CreateReader(str);
+                do
+                {
+                    while (reader.Read())
+                    {
+                        phoneBook.AddContact(reader.GetString(0), reader.GetString(1));
+                    }
+                } while (reader.NextResult());
+                phoneBook.table.Rows.RemoveAt(0);
+                str.Close();
+            }
+
+
+
+             public void Save (PhoneBook phoneBook, string fileName)
+            { }
+        
+    }
+}
